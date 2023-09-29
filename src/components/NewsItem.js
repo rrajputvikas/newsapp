98b4379188d6c052781e641491947537dedf464a
Vikas Rajput <vikassinghrajput551@gmail.com>
2023-09-29 19:23:30 +0530

    Fetching the data directly from NewsAPI and not using the Article.js because everyone want updated news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ class NewsItem extends Component {
     render() {
         const {title, description, imageUrl, newsUrl} = this.props;
         return(
-            <div className="my-4">
-                <div className="card" style={{width: "18rem"}}>
+            <div className="my-4 ">
+                <div className="card mx-auto" style={{width: "18rem"}}>
                     <img src={imageUrl} alt="News Updates" className="card-img-top" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-warning">Read More</a>
                     </div>
                 </div>
--- src/containers/News.js
+++ src/containers/News.js
@@ -1,34 +1,88 @@
 import React, { Component } from "react";
 import NewsItem from "../components/NewsItem";
-import articles from "../Articles";
-import defaultImage from '../imageNotAvailable.png'
+import defaultImage from "../imageNotAvailable.png";
 
 class News extends Component {
   constructor() {
     super();
     this.state = {
-      articles: articles,
+      articles: [],
+      page: 1,
+      totalResults: 0,
       loading: false,
     };
   }
 
+  apiKey = "Insert your API Key to run on your localhost";
+  async componentDidMount() {
+    const url =
+      `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.apiKey}&page=${this.state.page}&pageSize=15`;
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    this.setState({ 
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    });
+  }
+
+  prevClicked = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.apiKey}&page=${this.state.page - 1}&pageSize=15`;
+    const data = await fetch(url);
+    const parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1
+    });
+  }
+
+  nextClicked = async  () => {
+    if(this.state.page <= Math.ceil(this.state.totalResults / 15)) {
+      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.apiKey}&page=${this.state.page + 1}&pageSize=15`;
+      const data = await fetch(url);
+      const parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="container my-3">
-        <h2>News Company - Top Headlines</h2>
-        <div className="row">
-          {this.state.articles.map((element, index) => {
-            return (
-              <div className="col-md-4" key={index}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage===null? defaultImage: element.urlToImage}
-                  newsUrl={element.url}
-                ></NewsItem>
-              </div>
-            );
-          })}
+      <div>
+        <div className="container my-3">
+          <h2 className="text-center">News Company - Top Headlines</h2>
+          <div className="row">
+            {this.state.articles.map((element, index) => {
+              return (
+                <div className="col-md-auto" key={index}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 100) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage === null
+                        ? defaultImage
+                        : element.urlToImage
+                    }
+                    newsUrl={element.url}
+                  ></NewsItem>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="d-flex justify-content-end sticky-bottom">
+          <button disabled= {this.state.page<=1} type="button" onClick={this.prevClicked} className="btn btn-warning m-2">
+            &larr; Prev
+          </button>
+          <button disabled={this.state.page >= Math.ceil(this.state.totalResults/15)} type="button" onClick={this.nextClicked} className="btn btn-warning m-2">
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
